4f004e6b1ac3f37e8538a9df1c58a627b9ee2b01
Falk <julien.delusseau@gmail.com>
2020-09-02 10:39:52 +0200

    Added the logout function

--- client/src/layouts/Navbar.js
+++ client/src/layouts/Navbar.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { logout } from '../redux/actions/authActions'
+import PropTypes from 'prop-types'
 
-const Navbar = () => {
+const mapState = state => ({
+  auth: state.auth
+})
+
+const mapDispatch = { logout }
+
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
+    <ul>
+      <li>
+        <Link to='#!' onClick={logout}>
+          <i className='fas fa-sign-out-alt'></i> <span className='hide-sm'>Logout</span>
+        </Link>
+      </li>
+    </ul>
+  )
+
+  const guestLinks = (
+    <ul>
+      <li>
+        <Link to='/profiles'>Développeurs</Link>
+      </li>
+      <li>
+        <Link to='/register'>S'enregistrer</Link>
+      </li>
+      <li>
+        <Link to='/login'>Se connecter</Link>
+      </li>
+    </ul>
+  )
+
   return (
     <nav className='navbar bg-dark'>
       <h1>
@@ -9,19 +42,14 @@ const Navbar = () => {
           <i className='fas fa-code'></i> DevConnector
         </Link>
       </h1>
-      <ul>
-        <li>
-          <Link to='/profiles'>Développeurs</Link>
-        </li>
-        <li>
-          <Link to='/register'>S'enregistrer</Link>
-        </li>
-        <li>
-          <Link to='/login'>Se connecter</Link>
-        </li>
-      </ul>
+      {!loading && <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>}
     </nav>
   )
 }
 
-export default Navbar
+Navbar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+}
+
+export default connect(mapState, mapDispatch)(Navbar)
--- client/src/redux/reducers/authReducer.js
+++ client/src/redux/reducers/authReducer.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS } from '../types'
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS, LOGOUT } from '../types'
 
 const INIT_STATE = {
   token: localStorage.getItem('token'),
@@ -30,11 +30,14 @@ export default (state = INIT_STATE, action) => {
     case LOGIN_FAIL:
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGOUT:
+      localStorage.removeItem('token')
       return {
         ...state,
         token: null,
         isAuthenticated: false,
-        loading: false
+        loading: false,
+        user: null
       }
     default:
       return state
